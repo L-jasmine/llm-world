a752aefe2e5d1f7272d7014bfe19d5f4311a83ef
csh <458761603@qq.com>
2024-07-29 02:33:08 +0800

    Support input cursor & auto new line

--- src/component/chat.rs
+++ src/component/chat.rs
@@ -20,10 +20,10 @@ use crate::llm::local_llm::Token;
 pub struct MessagesComponent {
     contents: LinkedList<Content>,
     cursor: (u16, u16),
+    last_mouse_event: MouseEvent,
     lock_on_bottom: bool,
     pub(super) wait_token: bool,
     area: Rect,
-    last_mouse_event: MouseEvent,
     active: bool,
 }
 
@@ -49,6 +49,10 @@ impl MessagesComponent {
         if event.kind == MouseEventKind::Down(MouseButton::Left) {
             self.active = self.area.contains(Position::new(event.column, event.row))
         }
+        if event.kind == MouseEventKind::Drag(MouseButton::Left) && self.active {
+            self.move_scoll(event);
+        }
+        self.last_mouse_event = event;
     }
 
     pub fn render(&mut self, frame: &mut Frame, area: Rect)
@@ -68,8 +72,28 @@ impl MessagesComponent {
                 format!("{}:", content.role.to_string().to_uppercase()),
                 style,
             )]);
-            text.extend(Text::raw(&content.message).style(style));
-            text.extend([Line::default().style(style)]);
+            {
+                let chars = content.message.chars();
+                let max_len = (self.area.width.max(2) - 2) as usize;
+                let mut s = String::with_capacity(max_len);
+                let mut len = 0;
+                for c in chars {
+                    s.push(c);
+                    if c.is_ascii() {
+                        len += 1;
+                    } else {
+                        len += 2;
+                    }
+                    if len >= max_len {
+                        text.extend(Line::raw(s).style(style));
+                        s = String::with_capacity(max_len);
+                        len = 0;
+                    }
+                }
+                text.extend(Line::raw(s).style(style));
+                // text.extend(Text::raw(&content.message).style(style));
+                text.extend([Line::styled(format!("[{max_len},{len}]"), style)]);
+            }
         }
 
         let line_n = text.lines.len();
@@ -89,16 +113,23 @@ impl MessagesComponent {
         }
 
         let paragraph = Paragraph::new(text)
-            .block(if self.active {
-                Block::bordered()
-                    .title(format!("{:?}", self.cursor))
-                    .title_bottom("[scroll]")
-                    .gray()
-            } else {
-                Block::bordered().title(format!("{:?}", self.cursor)).gray()
-            })
+            .block(Block::bordered().title(format!("{:?}", self.cursor)).gray())
             .scroll(self.cursor);
         frame.render_widget(paragraph, area);
+    }
+
+    pub fn move_scoll(&mut self, event: MouseEvent) {
+        let (delta_y, delta_x) = (
+            event.row as i16 - self.last_mouse_event.row as i16,
+            event.column as i16 - self.last_mouse_event.column as i16,
+        );
+        if delta_x != 0 {
+            self.cursor.1 = (self.cursor.1 as i16 - delta_x).max(0) as u16;
+        }
+        if delta_y != 0 {
+            self.cursor.0 = (self.cursor.0 as i16 - delta_y).max(0) as u16;
+            self.lock_on_bottom = false;
+        }
     }
 
     pub fn handler_input(&mut self, input: Input) {
@@ -135,22 +166,8 @@ impl MessagesComponent {
                             self.lock_on_bottom = false;
                         }
                     }
-                    MouseEventKind::Drag(MouseButton::Left) if self.active => {
-                        let (delta_y, delta_x) = (
-                            event.row as i16 - self.last_mouse_event.row as i16,
-                            event.column as i16 - self.last_mouse_event.column as i16,
-                        );
-                        if delta_x != 0 {
-                            self.cursor.1 = (self.cursor.1 as i16 - delta_x).max(0) as u16;
-                        }
-                        if delta_y != 0 {
-                            self.cursor.0 = (self.cursor.0 as i16 - delta_y).max(0) as u16;
-                            self.lock_on_bottom = false;
-                        }
-                    }
                     _ => {}
                 }
-                self.last_mouse_event = event;
                 self.update_active(event);
             }
             _ => {}
@@ -162,6 +179,10 @@ pub struct ChatComponent {
     user_tx: crossbeam::channel::Sender<Message>,
     messages: MessagesComponent,
     input: TextArea<'static>,
+    cursor_delta: (i16, i16),
+    last_mouse_event: MouseEvent,
+    active: bool,
+    area: Rect,
     exit_n: u8,
     pub event: String,
     rewrite: bool,
@@ -172,9 +193,6 @@ pub enum Input {
     Event(Event),
     Message(Message),
 }
-
-// 新 chat
-// 重写 assistant
 
 impl ChatComponent {
     pub fn new(
@@ -188,7 +206,37 @@ impl ChatComponent {
             event: String::new(),
             user_tx,
             rewrite: false,
+            cursor_delta: (0, 0),
+            last_mouse_event: MouseEvent {
+                kind: MouseEventKind::Moved,
+                column: 0,
+                row: 0,
+                modifiers: KeyModifiers::empty(),
+            },
+            active: false,
+            area: Rect::default(),
         }
+    }
+
+    fn update_active(&mut self, event: MouseEvent) {
+        if event.kind == MouseEventKind::Down(MouseButton::Left) {
+            self.active = self.area.contains(Position::new(event.column, event.row))
+        }
+        if event.kind == MouseEventKind::Drag(MouseButton::Left) && self.active {
+            self.move_scoll(event)
+        } else {
+            self.cursor_delta = (0, 0);
+        }
+        self.last_mouse_event = event;
+    }
+
+    pub fn move_scoll(&mut self, event: MouseEvent) {
+        let (delta_y, delta_x) = (
+            event.row as i16 - self.last_mouse_event.row as i16,
+            event.column as i16 - self.last_mouse_event.column as i16,
+        );
+
+        self.cursor_delta = (delta_y, delta_x);
     }
 
     pub fn render(&mut self, frame: &mut Frame, area: Rect)
@@ -198,6 +246,8 @@ impl ChatComponent {
         let vertical = Layout::vertical([Constraint::Min(5), Constraint::Max(10)]);
         let [messages_area, input_area] = vertical.areas(area);
 
+        self.area = input_area;
+
         self.messages.render(frame, messages_area);
         if self.messages.wait_token {
             self.input
@@ -206,6 +256,8 @@ impl ChatComponent {
             self.input
                 .set_block(Block::bordered().title("Input").gray())
         }
+        self.input
+            .scroll((-self.cursor_delta.0, -self.cursor_delta.1));
         frame.render_widget(self.input.widget(), input_area);
     }
 
@@ -257,6 +309,8 @@ impl ChatComponent {
 
     pub fn handler_input<B: Backend>(&mut self, terminal: &mut Terminal<B>, input: Input) -> bool {
         self.event = format!("{:?}", input);
+        let is_event = matches!(&input, Input::Event(..));
+
         match input {
             Input::Event(Event::Key(input)) if input.code == KeyCode::F(5) => {
                 let _ = terminal.clear();
@@ -278,17 +332,27 @@ impl ChatComponent {
                 }
             }
             Input::Event(Event::Key(input)) if input.code == KeyCode::Esc => {
-                self.exit_n += 50;
-                return self.exit_n < 100;
+                self.exit_n += 2;
+                return self.exit_n < 3;
             }
             Input::Event(Event::Key(input)) => {
                 self.input.input(input);
+            }
+            Input::Event(Event::Mouse(event)) => {
+                self.update_active(event);
+                if !self.active {
+                    self.messages
+                        .handler_input(Input::Event(Event::Mouse(event)));
+                }
             }
             input => {
                 self.messages.handler_input(input);
             }
         }
-        self.exit_n = self.exit_n.max(1) - 1;
+
+        if is_event {
+            self.exit_n = self.exit_n.max(1) - 1;
+        }
         true
     }
 }
